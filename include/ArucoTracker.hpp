f7c55e2accd3d0cc97c20a6cd6af23f90513afa4
Philipp Gagel <philippgagel@ymail.com>
2021-03-17 02:12:28 +0100

    added Aruco Tracking. added Debug draws

--- include/ArucoTracker.hpp
+++ include/ArucoTracker.hpp
@@ -26,6 +26,8 @@ class ArucoTracker {
    public:
     ArucoTracker(const std::vector<int32_t>& ids,
                  const std::vector<Callback_t>& callbacks,
+                 float real_size,
+                 std::string camera_calib_file,
                  std::shared_ptr<ADataBackend> backend);
 
     void set_callback(int32_t id, Callback_t callback);
@@ -38,10 +40,23 @@ class ArucoTracker {
     std::vector<int32_t> m_marker_ids;
     // map containing all marker ids with the corrosponding callbacks
     std::vector<Callback_t> m_callbacks;
-    // access to data bases for retreiving whatever from information to markers
+    // Real size of markers (only one measurement required, since they are square
+    float m_real_size;
+    // access to data bases for retreiving whatever from information to markers_out
     std::shared_ptr<DataAccess> m_data_access;
-    // dictionary used bz opencv to get markers information
+    // dictionary used by opencv to get markers information
     cv::Ptr<cv::aruco::Dictionary> m_dictionary;
+    // aruco parameters for tracking line threshold and marker variance, border deadzone and so on
+    cv::Ptr<cv::aruco::DetectorParameters> m_parameters;
+    // moving median for translation and rotation
+    uint64_t m_moving_median_length;
+    std::vector<cv::Mat> m_moving_median_rotation;
+    std::vector<cv::Mat> m_moving_median_translation;
+
+
+    // Camera calibration variables
+    cv::Mat m_camera_matrix;
+    cv::Mat m_dist_coeffs;
 };
 
 }  // namespace amt
--- src/ArucoTracker.cpp
+++ src/ArucoTracker.cpp
@@ -1,17 +1,36 @@
 #include "ArucoTracker.hpp"
-
+#include <iostream>
 #include <algorithm>
+#include <opencv2/core.hpp>
 
 namespace amt {
 
 ArucoTracker::ArucoTracker(const std::vector<int32_t>& ids,
                            const std::vector<Callback_t>& callbacks,
+                           float real_size,
+                           std::string camera_calib_file,
                            std::shared_ptr<ADataBackend> backend)
     : m_marker_ids(ids),
       m_callbacks(callbacks),
+      m_real_size(real_size),
       m_dictionary(
           cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL)) {
     this->m_data_access.reset(new DataAccess(backend));
+    
+    // set detection parameters like theshold, mind marker size and so on
+    this->m_parameters = cv::aruco::DetectorParameters::create();
+    
+    this->m_moving_median_length = 10;
+
+    auto fs = cv::FileStorage(camera_calib_file, cv::FileStorage::READ);
+    std::cout << "real size: " << this->m_real_size << std::endl;
+    if(fs.isOpened()) {
+        fs["cameraMatrix"] >> this->m_camera_matrix;
+        fs["dist_coeffs"] >> this->m_dist_coeffs;
+    } else {
+        std::cout << "Camera callibration could not be loaded. Results may suffer." << std::endl;
+    }
+
 }
 
 void ArucoTracker::set_callback(int32_t id, Callback_t callback) {
@@ -57,20 +76,44 @@ void ArucoTracker::get_markers(std::vector<cv::Mat>& markers_out, int size) {
 void ArucoTracker::update(cv::Mat image) {
     // init variables for results
     std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+    std::vector<int32_t> detected_marker_ids;
 
-    // set detection parameters like theshold, mind marker size and so on
-    cv::Ptr<cv::aruco::DetectorParameters> parameters =
-        cv::aruco::DetectorParameters::create();
-
-    // actually detect markers
+    // detect markers
     cv::aruco::detectMarkers(image, this->m_dictionary, markerCorners,
-                             this->m_marker_ids, parameters,
+                             detected_marker_ids, this->m_parameters,
                              rejectedCandidates);
+    
+    // call callbacks for detected markers
+    for (uint64_t marker = 0; marker < markerCorners.size(); marker++) {
+        auto iter = std::find(this->m_marker_ids.begin(), this->m_marker_ids.end(), detected_marker_ids[marker]);
+        if (iter == this->m_marker_ids.end()) {
+            continue;
+        }
+        int index = iter - this->m_marker_ids.begin();
+        this->m_callbacks[index](detected_marker_ids[marker]);
+    }
+
+    cv::Mat outputImage = image.clone();
+    
+    // get orientation and translation of each marker, average them out and display result (+draw for debug)
+    if(markerCorners.size() > 0) {
+        std::vector<cv::Vec3d> object_points;
+        std::vector<cv::Vec3d> rotations;
+        std::vector<cv::Vec3d> translations;
+
+        // get rotations and translations of all markers
+        cv::aruco::estimatePoseSingleMarkers(markerCorners, this->m_real_size, this->m_camera_matrix, this->m_dist_coeffs, rotations, translations, object_points);
+        
+        for (uint64_t marker = 0; marker < markerCorners.size(); marker++) {  
+            cv::aruco::drawAxis(outputImage, this->m_camera_matrix, this->m_dist_coeffs, rotations[marker], translations[marker], 0.01);
+        }
+    }
 
     // draw image
-    cv::Mat outputImage = image.clone();
     cv::aruco::drawDetectedMarkers(outputImage, markerCorners,
-                                   this->m_marker_ids);
+                                   detected_marker_ids);
+
+    // draw result
     cv::imshow("Aruco Marker Tracker", outputImage);
 }
 
