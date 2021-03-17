2c32be9039bc5ff1045d73e8bfa95a291fc473ba
Philipp Gagel <philippgagel@ymail.com>
2021-03-17 02:12:56 +0100

    added webcam support. added main loop.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
 #include <sstream>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>    
+
 #include "ArucoTracker.hpp"
 #include "data/CSVBackend.hpp"
 
-void callback(int id) { std::cout << id << std::endl; }
+void callback(int id) { std::cout << id << " detected" << std::endl; }
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello World!" << std::endl;
-
+int main() {
+    int id = 1;
+    cv::VideoCapture cam(id);
+    // try opening camera
+    while (!cam.isOpened()) {
+        std::cout << "Failed to make connection to cam" << std::endl;
+        cam.open(id++);
+    }
+    
+    // create database backend
     std::shared_ptr<amt::CSVBackend> backend =
         std::make_shared<amt::CSVBackend>("testfile");
 
-    std::vector<int> marker_ids = {1, 2, 3, 4, 5};
+    // create tracker
+    std::vector<int> marker_ids = {757, 787, 827, 863, 971};
     amt::ArucoTracker tracker(
         marker_ids, {callback, callback, callback, callback, callback},
+        0.03f, "../res/cameraParameters.xml", 
         backend);
 
+    // create marker images
     std::vector<cv::Mat> markers;
     tracker.get_markers(markers, 200);
 
-    for (int marker = 0; marker < marker_ids.size(); marker++) {
+    // save marker images
+    for (uint64_t marker = 0; marker < marker_ids.size(); marker++) {
         std::stringstream ss("");
 
         ss << "./marker_" << marker_ids[marker] << ".png";
 
         std::cout << ss.str() << std::endl;
         cv::imwrite(ss.str(), markers[marker]);
+    }
+
+    // run main loop until <ESC> is pressed
+    cv::Mat image;
+    while(cv::waitKey(1) != 27) {
+        // get camera image
+        cam >> image;
+        // update aruco tracker
+        tracker.update(image);
     }
 
     return 0;
